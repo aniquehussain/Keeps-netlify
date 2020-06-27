a5ee1ca69fb17cb8f2ea32bb2b99f9872b7381bb
aniquehussain <47793113+aniquehussain@users.noreply.github.com>
2020-06-27 20:58:46 +0530

    Hosted on Netlify 1st V

---
+++ build/precache-manifest.ea39c31e7074b7c2976dc2e11a426bd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cbef18c689f93ea151e71af9db9682e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbe79c44e880a7417f85",
+    "url": "/static/js/2.d1b3733b.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/static/js/2.d1b3733b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd3151678a69ee96305f",
+    "url": "/static/js/main.757a8c51.chunk.js"
+  },
+  {
+    "revision": "099c00861044cc834104",
+    "url": "/static/js/runtime-main.31afb492.js"
+  }
+]);
